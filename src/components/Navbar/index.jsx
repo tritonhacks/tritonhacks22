8ff2841615ab07f4d792f08acee890557db00dae
Faris Ashai <farisashai@gmail.com>
2022-01-14 21:50:44 -0800

    clean up code

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,35 +1,33 @@
 import "./style.css";
 
-let keys = 0;
+const links = [
+  {
+    id: "about",
+    displayText: "About",
+  },
+  {
+    id: "projects",
+    displayText: "Projects",
+  },
+  {
+    id: "sponsors",
+    displayText: "Sponsors",
+  },
+];
 
 const Navbar = () => {
-  const links = [
-    {
-      id: "about",
-      displayText: "About",
-    },
-    {
-      id: "projects",
-      displayText: "Projects",
-    },
-    {
-      id: "sponsors",
-      displayText: "Sponsors",
-    },
-  ];
-
   return (
     <div className="navbar-container">
       <a href="/" className="navbar-title">
         TritonHacks
       </a>
       <div className="navbar-links">
-        {links.map((link) => (
+        {links.map((link, key) => (
           <a
             id={link.id}
             className="underline"
             href={`#${link.id}`}
-            key={`nav${keys++}`}
+            key={`nav${key}`}
           >
             {link.displayText}
           </a>
