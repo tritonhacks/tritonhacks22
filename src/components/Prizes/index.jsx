7822cf0965622dc72af5ed4a6d2696b7dbc2b576
Alvin Hsu <a1hsu@ucsd.edu>
2022-01-24 00:43:27 -0800

    Prizes component

--- src/components/Prizes/index.jsx
+++ src/components/Prizes/index.jsx
@@ -1,7 +1,26 @@
 import "./style.css";
 
 const Prizes = () => {
-  return <div></div>;
+  return (
+    <div id="wrapper">
+      <div id="prize-wrapper">
+
+        <div id="prize-content">
+            <h1 id="prize">Prize Categories</h1>
+            <div id="prize-line"></div>
+
+          <div id="prize-paragraphs">
+            <p className="prize-field">
+              To be annouced 
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id="tree"></div>
+      <div id="o-wv"></div>
+    </div>
+      )
+
 };
 
 export default Prizes;
