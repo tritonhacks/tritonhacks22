e0834ebda49ce56f9aa8b72f8a5c1c5fda8e388c
Elaine Chao <elaine.iy@outlook.com>
2022-01-26 12:26:07 -0800

    imported images

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,8 @@
 import "./style.css";
+import discord from "../../assets/discord.svg";
+import facebook from "../../assets/fb_icon.svg";
+import instagram from "../../assets/insta_icon.svg";
+import mail from "../../assets/mail_icon.svg";
 
 const Footer = () => {
   return (
@@ -10,23 +14,23 @@ const Footer = () => {
       </div>
         <div id="icon-box">
             <div id="discord" className="icon">
-              <a href="" target="_blank">
-                <img src="../../assets/discord.svg" alt="discord"/>
+              <a href="" target="_blank" rel="noreferrer">
+                <img src={discord} alt="discord"/>
               </a>
             </div>
             <div id="facebook" className="icon">
-              <a href="https://www.facebook.com/triton.hacks" target="_blank">
-                <img src="../../assets/fb_icon.svg" alt="facebook"/>
+              <a href="https://www.facebook.com/triton.hacks" target="_blank" rel="noreferrer">
+                <img src={facebook} alt="facebook"/>
               </a>
             </div>
             <div id="insta" className="icon">
-              <a href="https://www.instagram.com/ucsdtritonhacks/" target="_blank">
-                <img src="../../assets/insta_icon.svg" alt="instagram"/>
+              <a href="https://www.instagram.com/ucsdtritonhacks/" target="_blank" rel="noreferrer">
+                <img src={instagram} alt="instagram"/>
               </a>
             </div>
             <div id="mail" className="icon">
               <a href="mailto:tritonhacks@gmail.com">
-                <img src="../../assets/mail_icon.svg" alt="email"/>
+                <img src={mail} alt="email"/>
               </a>
             </div>
         </div>
