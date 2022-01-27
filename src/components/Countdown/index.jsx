861cc8ff3ac6bf0b609b9fa8894724f08c757c1b
Kailey Wong <kaileyswong@gmail.com>
2022-01-26 19:00:58 -0800

    updated countdown w react hooks

--- src/components/Countdown/index.jsx
+++ src/components/Countdown/index.jsx
@@ -1,64 +1,70 @@
 import "./style.css";
-
-const hackathonStart = new Date("May 21, 2022 9:00:00").getTime();
-
-// update countdown every second
-var x = setInterval(function() {
-
-  var now = new Date().getTime();
-
-  // find the time between now and the count down date
-  var timeLeft = hackathonStart - now;
-
-  // calculations for days, hours, minutes, seconds
-  var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-   // if count down is finished, just display 0s
-  if (timeLeft < 0) {
-    clearInterval(x);
-    days = 0;
-    hours= 0;
-    minutes = 0;
-    seconds = 0;
-  }
-
-  // display time left 
-  document.getElementById("days-num").innerHTML = days;
-  document.getElementById("hours-num").innerHTML = hours;
-  document.getElementById("minutes-num").innerHTML = minutes;
-  document.getElementById("seconds-num").innerHTML = seconds;
-
-}, 1000);
+import { useState, useEffect } from 'react';
 
 
 const Countdown = () => {
+
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    
+    // update countdown every second
+    const countdownTimer = setInterval(() => {
+      
+      const hackathonStart = new Date("May 21, 2022 9:00:00").getTime();
+      const now = new Date().getTime();
+  
+      // find the time between now and the count down date
+      const timeLeft = hackathonStart - now;
+  
+      // calculations for days, hours, minutes, seconds
+      var updatedDays = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      var updatedHours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var updatedMinutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      var updatedSeconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+      
+      // if countdown is finished, just display 0s
+      if (timeLeft < 0) {
+        updatedDays = 0;
+        updatedHours= 0;
+        updatedMinutes = 0;
+        updatedSeconds = 0;
+      }
+
+      setDays(updatedDays);
+      setHours(updatedHours);
+      setMinutes(updatedMinutes);
+      setSeconds(updatedSeconds);
+      
+    }, 1000);
+
+    return () => clearInterval(countdownTimer);
+  }, []);
+
   return <div id="countdown-wrapper">
       <h1 id="countdown">Time Until Hacking Starts</h1>
-      <div id="timer-wrapper">
         <div id="timer">
           <div class="timer-element">
-            <p id="days-num" class="num"></p>
-            <p class="text">DAYS</p>
+            <p id="days-num" class="num">{days}</p>
+            <p class="text">days</p>
           </div>
           <div class="timer-element">
-            <p id="hours-num" class="num"></p>
-            <p class="text">HOURS</p>
+            <p id="hours-num" class="num">{hours}</p>
+            <p class="text">hours</p>
           </div>
           <div class="timer-element">
-            <p id="minutes-num" class="num"></p>
-            <p class="text">MINUTES</p>
+            <p id="minutes-num" class="num">{minutes}</p>
+            <p class="text">minutes</p>
           </div>
           <div class="timer-element">
-            <p id="seconds-num" class="num"></p>
-            <p class="text">SECONDS</p>
+            <p id="seconds-num" class="num">{seconds}</p>
+            <p class="text">seconds</p>
           </div>
         </div>
       </div>
-      
-      </div>;
-};
+  };
 
 export default Countdown;
