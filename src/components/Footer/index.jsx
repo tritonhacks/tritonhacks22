7d2c410aa1c1f3cd254b3c7aaec73ae45b6affba
Elaine Chao <elaine.iy@outlook.com>
2022-01-27 16:28:26 -0800

    fixed div/naming/spacing/size issues

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,33 +7,23 @@ import mail from "../../assets/mail_icon.svg";
 const Footer = () => {
   return (
     <div id="footer">
-      <div>
-        <h1 id="h1">
-          Contact Us
-        </h1>
+      <h1 id="foot-header">
+        Contact Us
+      </h1>
+      <div id="icon-box">
+        <a href="#footer" target="_blank" rel="noreferrer" className="icon">
+          <img src={discord} alt="discord"/>
+        </a>
+        <a href="https://www.facebook.com/triton.hacks" target="_blank" rel="noreferrer" className="icon">
+          <img src={facebook} alt="facebook"/>
+        </a>
+        <a href="https://www.instagram.com/ucsdtritonhacks/" target="_blank" rel="noreferrer" className="icon">
+          <img src={instagram} alt="instagram"/>
+        </a>
+        <a href="mailto:tritonhacks@gmail.com" className="icon">
+          <img src={mail} alt="email"/>
+        </a>
       </div>
-        <div id="icon-box">
-            <div id="discord" className="icon">
-              <a href="#footer" target="_blank" rel="noreferrer">
-                <img src={discord} alt="discord"/>
-              </a>
-            </div>
-            <div id="facebook" className="icon">
-              <a href="https://www.facebook.com/triton.hacks" target="_blank" rel="noreferrer">
-                <img src={facebook} alt="facebook"/>
-              </a>
-            </div>
-            <div id="insta" className="icon">
-              <a href="https://www.instagram.com/ucsdtritonhacks/" target="_blank" rel="noreferrer">
-                <img src={instagram} alt="instagram"/>
-              </a>
-            </div>
-            <div id="mail" className="icon">
-              <a href="mailto:tritonhacks@gmail.com">
-                <img src={mail} alt="email"/>
-              </a>
-            </div>
-        </div>
     </div>
   );
 };
