0bb39e9045d2f0c03a712a1048eb266632210125
Alvin Hsu <77023523+alvinhsu11@users.noreply.github.com>
2022-01-28 23:14:18 -0800

    Prizes component (#7)
    * Prizes component
    
    * updated Prizes files
    
    * fixed type

--- src/components/Prizes/index.jsx
+++ src/components/Prizes/index.jsx
@@ -1,7 +1,18 @@
 import "./style.css";
 
 const Prizes = () => {
-  return <div></div>;
+  return (
+     <div id="prize-wrapper">
+      <div id="prize-content">
+           <h1 id="prize">Prize Categories</h1>
+        <div id="prize-paragraphs">
+           <p className="prize-field">
+            To be announced 
+          </p>
+        </div>
+      </div>
+    </div>
+      )
 };
 
 export default Prizes;
