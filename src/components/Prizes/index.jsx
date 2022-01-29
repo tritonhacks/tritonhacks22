83fd787c76ff3cd746a5be2a5427a2fb6d5c0f5f
Alvin Hsu <a1hsu@ucsd.edu>
2022-01-28 22:48:38 -0800

    updated Prizes files

--- src/components/Prizes/index.jsx
+++ src/components/Prizes/index.jsx
@@ -2,25 +2,17 @@ import "./style.css";
 
 const Prizes = () => {
   return (
-    <div id="wrapper">
-      <div id="prize-wrapper">
-
-        <div id="prize-content">
-            <h1 id="prize">Prize Categories</h1>
-            <div id="prize-line"></div>
-
-          <div id="prize-paragraphs">
-            <p className="prize-field">
-              To be annouced 
-            </p>
-          </div>
+     <div id="prize-wrapper">
+      <div id="prize-content">
+           <h1 id="prize">Prize Categories</h1>
+        <div id="prize-paragraphs">
+           <p className="prize-field">
+            To be annouced 
+          </p>
         </div>
       </div>
-      <div id="tree"></div>
-      <div id="o-wv"></div>
     </div>
       )
-
 };
 
 export default Prizes;
