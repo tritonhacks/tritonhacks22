0035ff63e584114d83167c2f4079c3fbc35da1ea
Kailey Wong <kaileyswong@gmail.com>
2022-02-07 19:31:00 -0800

    fixed spacing + removed unnecessary divs

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -12,30 +12,24 @@ import vgdc from "../../assets/vgdc.png";
 const Sponsors = () => {
   return (
     <div id="sponsors-wrapper">
-      <div id="sponsors-container">
-        <h1 id="sponsors">Sponsors
-        </h1>
-        <p id="sponsor-us">Interested in sponsoring us? Email us at&nbsp;
-          <span>
-            <a id="email" href="mailto:tritonhacks@gmail.com">tritonhacks@gmail.com</a>
-          </span>.
-        </p>
-        <div id="sponsor-images">
-          <img src={ucsdjsoe} alt="UCSD Jacobs School of Engineering"></img>
-          <img src={deckard} alt="Deckard Technologies"></img>
-        </div>
-
+      <h1 id="sponsors">Sponsors
+      </h1>
+      <p id="sponsor-us">Interested in sponsoring us? Email us at&nbsp;
+        <span>
+          <a id="email" href="mailto:tritonhacks@gmail.com">tritonhacks@gmail.com</a>
+        </span>.
+      </p>
+      <div id="sponsor-images">
+        <img src={ucsdjsoe} alt="UCSD Jacobs School of Engineering"></img>
+        <img src={deckard} alt="Deckard Technologies"></img>
       </div>
-      <div id="partners-container">
-        <h1 id="partners">Partners</h1>
-        <div id="partner-images">
-          <img src={csforeach} alt="CSForEach"></img>
-          <img src={acm} alt="Association for Computing Machinery"></img>
-          <img src={dsss} alt="Data Science Student Society"></img>
-          <img src={ieee} alt="Institute of Electrical and Electronics Engineers"></img>
-          <img src={vgdc} alt="Video Game Development Club"></img>
-        </div>
-
+      <h1 id="partners">Partners</h1>
+      <div id="partner-images">
+        <img src={csforeach} alt="CSForEach"></img>
+        <img src={acm} alt="Association for Computing Machinery"></img>
+        <img src={dsss} alt="Data Science Student Society"></img>
+        <img src={ieee} alt="Institute of Electrical and Electronics Engineers"></img>
+        <img src={vgdc} alt="Video Game Development Club"></img>
       </div>
     </div>
   );
