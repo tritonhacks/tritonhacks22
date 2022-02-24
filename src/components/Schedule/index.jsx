0f5f6d7fd8a324e7507985710586cf0d7fde9755
Elaine Chao <elaine.iy@outlook.com>
2022-02-23 23:18:25 -0800

    fixed background

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -8,8 +8,8 @@ const Schedule = () => {
                 <h1>Schedule</h1>
             </div>
             <div id="calendar">
-                <iframe title="calendar" src="https://calendar.google.com/calendar/embed?wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=Y19qcDlyM3J2dTJhOXRoYWE4am03a3Rodmdha0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688" 
-                 width="800" height="600" frameborder="0" scrolling="no"/>  
+                 <iframe title="calendar" src="https://calendar.google.com/calendar/embed?wkst=1&bgcolor=%23f3ecdc&ctz=America%2FLos_Angeles&mode=WEEK&src=Y19qcDlyM3J2dTJhOXRoYWE4am03a3Rodmdha0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688" 
+                width="800" height="600" frameborder="0" scrolling="no"/> 
             </div>
         </div>
         </div>
