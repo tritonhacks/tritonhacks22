8d97714a5ae138deb2236965cc90ca69a699c78d
Elaine Chao <elaine.iy@outlook.com>
2022-02-24 18:04:33 -0800

    removed extra divs

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -2,16 +2,12 @@ import "./style.css";
 
 const Schedule = () => {
     return(
-        <div>
         <div id="schedule">
-            <div id="text">
-                <h1>Schedule</h1>
-            </div>
+            <h1 id="text">Schedule</h1>
             <div id="calendar">
                  <iframe title="calendar" src="https://calendar.google.com/calendar/embed?wkst=1&bgcolor=%23f3ecdc&ctz=America%2FLos_Angeles&mode=WEEK&src=Y19qcDlyM3J2dTJhOXRoYWE4am03a3Rodmdha0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688" 
                 width="800" height="600" frameborder="0" scrolling="no"/> 
             </div>
-        </div>
         </div>
     );
 }
