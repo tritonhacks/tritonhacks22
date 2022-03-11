6bab268b4a57af8da555747361ad35623c48a9af
Elaine Chao <elaine.iy@outlook.com>
2022-03-10 18:51:59 -0800

    added margins and changed h1 to id in css

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -5,7 +5,7 @@ const Schedule = () => {
         <div id="schedule">
             <h1 id="text">Schedule</h1>
             <div id="calendar">
-                 <iframe title="calendar" src="https://calendar.google.com/calendar/embed?wkst=1&bgcolor=%23f3ecdc&ctz=America%2FLos_Angeles&mode=WEEK&src=Y19qcDlyM3J2dTJhOXRoYWE4am03a3Rodmdha0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688" 
+                 <iframe title="calendar" id="calendar-iframe" src="https://calendar.google.com/calendar/embed?wkst=1&bgcolor=%23f3ecdc&ctz=America%2FLos_Angeles&mode=WEEK&src=Y19qcDlyM3J2dTJhOXRoYWE4am03a3Rodmdha0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688" 
                 width="800" height="600" frameborder="0" scrolling="no"/> 
             </div>
         </div>
